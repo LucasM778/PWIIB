8379976fcc33fa7af28089854c4820900145cd9f
Lab05_etec <Lab05_etec@etec>
2025-04-08 20:45:30 -0300

    criação da paginas de navegação rodape e cabecalho

--- formulario_exercicio.php
+++ formulario_exercicio.php
@@ -1,14 +1,5 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="bootstrap.min.css">
-    <title>Formulario EXE</title>
-</head>
-<body>
 
+<?php include "cabecalho.php" ?>
 
 <style>
   .Exercicio1{
@@ -16,44 +7,10 @@
     padding: 20px;
     text-align:center;
     font-size:arial;
-
-
   }
-</style>
-<nav class="navbar navbar-expand-lg bg-dark " data-bs-theme="dark">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#"> 
-    <img src= "\PWIIB\imagens\bootstrap-logo.svg " alt="Logo" width="30" height="24" class="d-inline-block align-text-top"> PWIIB
-    </a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-           php
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="variavel.php">Variavel</a></li>
-            <li><a class="dropdown-item" href="if.php">If</a></li>
-            <li><a class="dropdown-item" href="while.php">While</a></li>
-            <li><a class="dropdown-item" href="for.php">For</a></li>
-            <li><a class="dropdown-item" href="switch.php">Switch</a></li>
-            <li><a class="dropdown-item" href="array.php">Array/Vetor</a></li>
-            <li><a class="dropdown-item" href="formulario.php">formulario</a></li>
-            <li><a class="dropdown-item" href="index.php">Index</a></li>
-            <li><a class="dropdown-item" href="formulario_exercicio.php">formulario Exercicios</a></li>
-            <li><a class="dropdown-item" href="login.php">Login</a></li>
-          </ul>
-       
-      </ul>
-      
-    </div>
-  </div>
 
-</nav>
+
+</style>
 <div class= "Exercicio1" >
 <h1>Cadastro</h1>
 <h2>Exercicio 1</h2>
@@ -124,6 +81,4 @@
 </div>
 
 
-<script src="bootstrap.bundle.min.js"></script> 
-</body>
-</html>
+<?php include "rodape.php" ?>
--- login.php
+++ login.php
@@ -1,57 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="bootstrap.min.css">
-    <title>Login</title>
-</head>
-<body>
+<?php include "cabecalho.php" ?>
 
-<style>
-.a{
-
-
-
-}
 
 </style>
-
-<nav class="navbar navbar-expand-lg bg-dark " data-bs-theme="dark">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#"> 
-    <img src= "\PWIIB\imagens\bootstrap-logo.svg " alt="Logo" width="30" height="24" class="d-inline-block align-text-top"> PWIIB
-    </a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-           php
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="variavel.php">Variavel</a></li>
-            <li><a class="dropdown-item" href="if.php">If</a></li>
-            <li><a class="dropdown-item" href="while.php">While</a></li>
-            <li><a class="dropdown-item" href="for.php">For</a></li>
-            <li><a class="dropdown-item" href="switch.php">Switch</a></li>
-            <li><a class="dropdown-item" href="array.php">Array/Vetor</a></li>
-            <li><a class="dropdown-item" href="formulario.php">formulario</a></li>
-            <li><a class="dropdown-item" href="index.php">Index</a></li>
-            <li><a class="dropdown-item" href="login.php">Login</a></li>
-            <li><a class="dropdown-item" href="formulario_exercicio.php">formulario Exercicios</a></li>
-          </ul>
-       
-      </ul>
-      
-    </div>
-  </div>
-
-</nav>   
+  
     <div class= "pt-4 pb-4">
    <div class=" col-4 offset-4 container text-center bg-primary rounded  "  >
     
@@ -63,7 +14,7 @@
       <form action="verifica_login.php" method="post">
         <br>
         <div class = "form-floating ">
-          <input type="text" class="form-control" id="login" name="login" placeholder="Digite seu login"/>
+          <input type="text" class="form-control " id="login" name="login" placeholder="Digite seu login"/>
           <label for="login">Digite seu login</label> 
         </div>
           <br>
@@ -105,6 +56,4 @@
 
 
 
-<script src="bootstrap.bundle.min.js"></script>
-</body>
-</html>
+<?php include "rodape.php" ?>
